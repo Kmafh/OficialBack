4a41d63887309fb1c0e2006852cc11422489a0ce
Antonio Herrera <artadapt@gmail.com>
2023-11-20 22:31:44 +0100

    deployapelo con try

--- database/config.js
+++ database/config.js
@@ -6,7 +6,7 @@ const dbConnection = async () => {
         await mongoose.connect(process.env.DB_CNN);
             console.log("Conection BD OK")
     } catch (error) {
-        console.log("ERROR al conectar con BD")
+        console.log("ERROR al conectar con BD. "+error)
         // throw new Error("ERROR al conectar con BD")
         }
    
